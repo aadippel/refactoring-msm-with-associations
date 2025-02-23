8b3806e61df47b7c228698f29bcd390f069178c3
aadippel <aadippel@gmail.com>
2025-02-23 03:27:25 +0000

    finalized code

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,4 +15,16 @@
 class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
+
+  belongs_to(:director, :foreign_key => "director_id", :class_name => "Director")
+  belongs_to(:actor, :foreign_key => "actor_id", :class_name => "Actor")
+
+  has_many(:characters, :foreign_key => "movie_id", :class_name => "Character")
+  has_many(:cast, :through => :characters, :source => :actor)
+
+  # def director
+  #   x = self.director_id
+
+  #   return Director.where({ :id => x }).first
+  # end
 end
